8af8b8be6997486968be2525890d670a6034a58a
benvredenburg <vre19001@byui.edu>
2020-10-12 16:26:24 -0600

    10/12

---
+++ inclass.py
@@ -0,0 +1,12 @@
+# Ask user question.
+
+location = input('Where is the game played? ')
+
+# Compare input to a location.
+
+if location.capitalize() != 'Washington':
+    print('This is an away game.')
+else:
+    print('This is a home game.')
+
+print(location.capitalize() != 'Washington')
