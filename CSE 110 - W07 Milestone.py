97a327095917ca3ef1c991c3a669f9b6869cea02
benvredenburg <vre19001@byui.edu>
2020-11-04 13:06:17 -0700

    11/04

--- CSE 110 - W07 Milestone.py
+++ CSE 110 - W07 Milestone.py
@@ -1,19 +1,21 @@
 from PIL import Image
 
-image_beach = Image.open('images/beach.jpg')
+image_desert = Image.open('images/desert.jpg')
 
-pixels_beach = image_beach.load()
-(width, height) = image_beach.size
 
+pixels_desert = image_desert.load()
+
+(width, height) = image_desert.size
 
 for x in range(0, width):
     for y in range(0, height):
-        (r, g, b) = pixels_beach[x, y]
+        (r, g, b) = pixels_desert[x, y]
 
         new_blue = b + 100
         new_red = r + 100
         new_green = g + 100
 
-        pixels_beach[x, y] = (new_red, g, b)
+        pixels_desert[x, y] = (new_red, g, b)
 
-image_beach.show()
+image_desert.show()
+
---
+++ CSE 110 - W08 Milestone.py
@@ -0,0 +1,61 @@
+from PIL import Image
+
+image_desert = Image.open('images/desert.jpg')
+image_cactus = Image.open('images/cactus.jpg')
+image_cat = Image.open('images/cat_small.jpg')
+image_hiker = Image.open('images/hiker.jpg')
+
+pixels_desert = image_desert.load()
+pixels_cactus = image_cactus.load()
+pixels_cat = image_cat.load()
+pixels_hiker = image_hiker.load()
+
+(width, height) = image_desert.size
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r, g, b) = pixels_cat[x, y]
+
+        if g < 150:
+            new_blue = b + 100
+            new_red = r + 100
+            new_green = g + 100
+
+            pixels_cat[x, y] = (new_red, g, b)
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r, g, b) = pixels_hiker[x, y]
+
+        if g < 150:
+            new_blue = b + 100
+            new_red = r + 100
+            new_green = g + 100
+
+            pixels_hiker[x, y] = (r, g, new_blue)
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r, g, b) = pixels_cactus[x, y]
+
+        if r < 150 and b < 150 and g > 100:
+            pixels_cactus[x, y] = pixels_hiker[x - 100, y]
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r, g, b) = pixels_cactus[x, y]
+
+        if r < 150 and b < 150 and g > 100:
+            pixels_cactus[x, y] = pixels_cat[x, y]
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r, g, b) = pixels_cactus[x, y]
+
+        if r < 150 and b < 150 and g > 100:
+            pixels_cactus[x, y] = pixels_desert[x, y]
+
+
+
+image_cactus.save('W08Milestone.jpg')
+# image_cactus.show()
